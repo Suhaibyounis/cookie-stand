a5248dc2a74810c040d21958a1b1c3cadfe79daa
Suhaibyounis <younissuhaib@yahoo.com>
2021-02-23 13:34:27 +0200

    modify header function

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -5,81 +5,26 @@ let dailyLocationTotal =[];
 
 const table = document.getElementById('table'), totalPerHour =0;
 
-const tableElement = document.createElement('table');
+const tableElement = document.createElement('table')
    table.appendChild(tableElement);
 
     const tr1Element = document.createElement('tr');
     tableElement.appendChild(tr1Element);
 
+    const th1 = document.createElement('th');
+    tr1Element.appendChild(th1);
+    th1.textContent = 'Location';
 
-    const th1Element = document.createElement('th');
-    tr1Element.appendChild(th1Element);
-    th1Element.textContent = '';
-
-    const th2Element = document.createElement('th');
-    tr1Element.appendChild(th2Element);
-    th2Element.textContent = '6:00 am';
-
-    const th3Element = document.createElement('th');
-    tr1Element.appendChild(th3Element);
-    th3Element.textContent = '7:00 am';
-  
-    const th4Element = document.createElement('th');
-    tr1Element.appendChild(th4Element);
-    th4Element.textContent = '8:00 am'
-
-    const th5Element = document.createElement('th');
-    tr1Element.appendChild(th5Element);
-    th5Element.textContent = '9:00 am';
-
-    const th6Element = document.createElement('th');
-    tr1Element.appendChild(th6Element);
-    th6Element.textContent = '10:00 am';
-  
-    const th7Element = document.createElement('th');
-    tr1Element.appendChild(th7Element);
-    th7Element.textContent = '11:00 am'
-
-    const th8Element = document.createElement('th');
-    tr1Element.appendChild(th8Element);
-    th8Element.textContent = '12:00 pm';
-
-    const th9Element = document.createElement('th');
-    tr1Element.appendChild(th9Element);
-    th9Element.textContent = '1:00 pm';
-  
-    const th10Element = document.createElement('th');
-    tr1Element.appendChild(th10Element);
-    th10Element.textContent = '2:00 pm'
-
-    const th11Element = document.createElement('th');
-    tr1Element.appendChild(th11Element);
-    th11Element.textContent = '3:00 pm';
-
-    const th12Element = document.createElement('th');
-    tr1Element.appendChild(th12Element);
-    th12Element.textContent = '4:00 pm';
-  
-    const th13Element = document.createElement('th');
-    tr1Element.appendChild(th13Element);
-    th13Element.textContent = '5:00 pm'
-
-    const th14Element = document.createElement('th');
-    tr1Element.appendChild(th14Element);
-    th14Element.textContent = '6:00 pm';
-
-    const th15Element = document.createElement('th');
-    tr1Element.appendChild(th15Element);
-    th15Element.textContent = '7:00 pm';
-  
-    const th16Element = document.createElement('th');
-    tr1Element.appendChild(th16Element);
-    th16Element.textContent = 'Total'
+    for (let i= 0; i < workingHours.length; i++){
+      const th2 = document.createElement('th');
+      tr1Element.appendChild(th2);
+      th2.textContent = workingHours[i];
+    
+    }
+    const th3 = document.createElement('th');
+      tr1Element.appendChild(th3);
+      th3.textContent = 'Total of Total';
    
-
-
-// Seattle
-
 function SalmonStand (city, minCust, maxCust, avgCookies){
     this.city = city;
     this.minCust = minCust;
@@ -104,19 +49,12 @@ dailyLocationTotal.push(this.total);
 console.log(dailyLocationTotal);
 };
 
-
-
 SalmonStand.prototype.getCustPerHour = function (minCust, maxCust){
     for (let i = 0; i < workingHours.length; i++) {
         let custmer = generateRandomNumber(this.minCust, this.maxCust);
         this.custPerHour.push(custmer);
  }
 };
-
-
-
-
-
 
 SalmonStand.prototype.render = function(){
     const parentElement = document.getElementById('salesInfo');
